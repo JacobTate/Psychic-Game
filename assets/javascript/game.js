3315f76fff67e420100a9475f84967bd4679ea05
Jacob Tate <jacob87608@gmail.com>
2019-12-28 09:07:54 -0600

    got the basic function of the game down

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,61 @@
-//detects when a key is pressed
- document.onkeyup = function(event){
-     
-//the letters to pick from
+   //the letters to pick from
 var letter_array = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-for (var i = 0; i < letter_array.length; i++) {
-    //the computer guess picks a random item from the array
-var computer_guess = letter_array[Math.floor(Math.random()* letter_array.length)];
+var gusses = 9;
+var wins = 0;
+var losses = 0;
+var picked_letter = [];   
+
+ //the computer guess picks a random item from the array
+  var get_random_item = function() {
+      
+     random_item = letter_array[Math.floor(Math.random()* letter_array.length)];
+     //logs the computer guess
+    console.log(random_item)
+    return random_item
 }
-//logs the computer guess
-console.log(computer_guess)
-   //determines what key was pressed
-    var user_guess = event.key;
-    //if the pressed key = the random key alert
-    if(user_guess === computer_guess){
-        alert("you win")
-       ;
-    }
-    else{
-       
-    }
-    
+//resets the guessed letters and picks a new random letter
+var reset = function(){
+    computer_guess = get_random_item();
+    gusses = 9;
+}
+
+//stores the functions output in a variable
+var computer_guess = get_random_item();
+
+//detects when a key is pressed
+document.onkeyup = function(event){
+    gusses--
+    //determines what key was pressed
+  var user_guess = event.key;
+   //if the pressed key = the random key 
+if(user_guess === computer_guess){
+    alert("you win");  
+    //runs the reset function
+    reset();  
+    wins++
+}
+if(gusses === 0){
+    alert("you lost")
+    //runs the reset function
+    reset();
+    losses++
+    picked_letter.push(user_guess);
+}
 };
+document.getElementById("gamediv")
+
+
+
+
+
+
+
+ 
+    
+    
+  
+        
+
+    
+
 
