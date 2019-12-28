4fc3a68086e27fde8856dd78a978f217de7bdc36
Jacob Tate <jacob87608@gmail.com>
2019-12-28 10:36:28 -0600

    the basic game is finished starting on css and detail

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,78 +1,91 @@
-   //the letters to pick from
+//the letters to pick from
 var letter_array = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 //gusses = the number of guesses you get
 var guesses = 9;
 //setting the values to 0
 var wins = 0;
 var losses = 0;
-var guesses_so_far = 0;
-var picked_letter = [];   
+var picked_letter = [];
 
+//gets the element by id for adding text
 var wins_text = document.getElementById("winstext")
 var losses_text = document.getElementById("lossestext")
+var guesses_text = document.getElementById("guessestext") 
+var guessed_letter = document.getElementById("guessedletter")
 
+//the computer guess picks a random item from the array
+var get_random_item = function () {
 
-
-
-
- //the computer guess picks a random item from the array
-  var get_random_item = function() {
-      
-     random_item = letter_array[Math.floor(Math.random()* letter_array.length)];
-     //logs the computer guess
+    random_item = letter_array[Math.floor(Math.random() * letter_array.length)];
+    //logs the computer guess
     console.log(random_item)
     return random_item
 }
 //resets the guessed letters and picks a new random letter
-var reset = function(){
+var reset = function () {
     computer_guess = get_random_item();
-    gusses = 9;
+    guesses = 10;
+    picked_letter = [];
 }
 
 //stores the functions output in a variable
 var computer_guess = get_random_item();
 
 //detects when a key is pressed
-document.onkeyup = function(event){
-    guesses--
-    //determines what key was pressed
-  var user_guess = event.key;
-   //if the pressed key = the random key 
-if(user_guess === computer_guess){
-    alert("you win");  
-    //runs the reset function
-    reset();
-
-    wins++
+document.onkeyup = function (event) {
     
-    wins_text.innerHTML = "wins: " + wins
-}
-if(guesses === 0){
-    alert("you lost")
-    //runs the reset function
-    reset();
-
-    losses++
-
-losses_text.innerHTML = "losses:" + losses
-
+    //removes 1 guess
+    guesses--
+    
+    //prints the nubmer of remaining guesses
+    guesses_text.innerHTML = "guesses left: " + guesses
+   
+    //determines what key was pressed
+    var user_guess = event.key;
+  
     picked_letter.push(user_guess);
-}
+    guessed_letter.innerHTML = "your guess: " + picked_letter.join(' ');
+    //if the pressed key = the random key   
+    if (user_guess === computer_guess) {
+        
+        //runs the reset function
+        reset();
+//adds 1 to the wins
+        wins++
+
+//prints the number of wins to the screen
+        wins_text.innerHTML = "wins: " + wins
+
+    }
+
+    if (guesses === 0) {
+
+        //runs the reset function
+        reset();
+
+        //adds 1 to the losses
+        losses++
+
+//prints the number of losses to the screen
+        losses_text.innerHTML = "losses:" + losses
+
+        
+    }
 };
 
-   
 
 
 
 
 
 
- 
-    
-    
-  
-        
 
-    
+
+
+
+
+
+
+
 
 
